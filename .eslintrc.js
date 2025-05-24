b7b7ccb5b829cc595e9c7e87bdb49bac4255f0c1
Dan Bayan <danbayan@protonmail.com>
2025-05-24 16:44:24 +0200

    eslintrc errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,16 @@
+// .eslintrc.js
 module.exports = {
   extends: "next/core-web-vitals",
   rules: {
-    // Downgrade these specific errors to warnings
-    "@typescript-eslint/no-unused-vars": "warn",
-    "@typescript-eslint/no-explicit-any": "warn",
-    "react-hooks/exhaustive-deps": "warn"
-  }
+    // Turn off the unused vars rule completely
+    "@typescript-eslint/no-unused-vars": "off",
+    
+    // Turn off explicit any warnings
+    "@typescript-eslint/no-explicit-any": "off",
+    
+    // Turn off react hooks exhaustive deps warnings
+    "react-hooks/exhaustive-deps": "off"
+  },
+  // This ignores specific files or directories
+  ignorePatterns: ["node_modules/", ".next/", "public/"]
 }
